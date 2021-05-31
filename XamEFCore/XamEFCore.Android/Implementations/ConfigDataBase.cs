8a1f62e8fe4d0da613d02eefcc36595852b5e879
Your Name <you@example.com>
2021-05-31 16:30:59 -0500

    first commit

---
+++ XamEFCore/XamEFCore.Android/Implementations/ConfigDataBase.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Xamarin.Forms;
+using XamEFCore.Droid.Implementations;
+using XamEFCore.Interfaces;
+
+[assembly: Dependency(typeof(ConfigDataBase))]
+namespace XamEFCore.Droid.Implementations
+{
+    public class ConfigDataBase : IConfigDataBase
+    {
+        public string GetFullPath(string databaseFileName)
+        {
+            return Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), databaseFileName);
+        }
+    }
+}
+
